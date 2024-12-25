f47a51ca611f8a0e3eb1d3e4a9858bd802f4d0d1
seunghoon <zlzlzlmo@daum.net>
2024-12-26 03:07:24 +0900

    최신회차 정보 렌더링

--- src/pages/home/card/Card.tsx
+++ src/pages/home/card/Card.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import styles from "./Card.module.scss";
+import { useRounds } from "../../../context/rounds";
+import { formatNumberWithCommas } from "../../../utils/number";
 
 const Card = () => {
+  const { latestRound, isLoading, error } = useRounds();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error || !latestRound) return <div>Error: {error}</div>;
+
   return (
     <div className={styles.cardContainer}>
       <div className={styles.card}>
         <div className={styles.cardHeader}>
-          <span className={styles.round}>1056회 당첨번호 [2월 25일 기준]</span>
+          <span className={styles.round}>
+            {latestRound.drawNumber}회 당첨번호 [{latestRound.date}]
+          </span>
           <span className={styles.result}>[당첨결과 +]</span>
         </div>
         <div className={styles.numbersContainer}>
-          {[13, 20, 24, 32, 36, 45, "+29"].map((num, index) => (
+          {[
+            ...latestRound.winningNumbers, // 당첨 번호
+            `+${latestRound.bonusNumber}`, // 보너스 번호
+          ].map((num, index) => (
             <div
               key={index}
               className={`${styles.number} ${index === 6 ? styles.bonus : ""}`}
@@ -19,7 +31,14 @@ const Card = () => {
             </div>
           ))}
         </div>
-        <div className={styles.prizeInfo}>1등 14명 | 1,969,662,456원</div>
+        <div className={styles.prizeInfo}>
+          1등{" "}
+          {formatNumberWithCommas(
+            latestRound.prizeStatistics.firstPrizeWinnerCount
+          )}
+          명 |{" "}
+          {formatNumberWithCommas(latestRound.prizeStatistics.firstWinAmount)}원
+        </div>
       </div>
     </div>
   );
