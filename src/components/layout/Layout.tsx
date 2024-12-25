1bb4d3930e24a7af95c8a02daa06bc580acec976
seunghoon <zlzlzlmo@daum.net>
2024-12-26 06:33:30 +0900

    Layout 코드 정리

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,24 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import styles from "./Layout.module.scss";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-import { useLottoNumber } from "../../context/lottoNumbers";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const { resetExcludedNumbers } = useLottoNumber();
-
-  useEffect(() => {
-    // 페이지 전환 시 제외 번호 초기화
-    return () => {
-      resetExcludedNumbers();
-    };
-  }, [resetExcludedNumbers]);
-
   return (
     <div className={styles.layout}>
       <Header />
