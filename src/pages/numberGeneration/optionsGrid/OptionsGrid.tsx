99cdd8f9376aa3a9b22e3170d59bad3b84fba3ea
seunghoon <zlzlzlmo@daum.net>
2024-12-26 13:11:11 +0900

    팝업 기능구현

--- src/pages/numberGeneration/optionsGrid/OptionsGrid.tsx
+++ src/pages/numberGeneration/optionsGrid/OptionsGrid.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./OptionsGrid.module.scss";
 import { useNavigate } from "react-router-dom";
-import { useLottoNumber } from "../../../context/lottoNumbers";
-import { useRounds } from "../../../context/rounds";
+import PopupModal from "../../../components/common/popup/PopupModal";
+import useOptionsGrid from "./hooks/useOptionsGrid";
 
 // 옵션 타입 정의
 interface Option {
@@ -14,8 +14,9 @@ interface Option {
 
 const OptionsGrid: React.FC = () => {
   const navigate = useNavigate();
-  const { handleRequiredNumbers } = useLottoNumber();
-  const { getRecentRounds } = useRounds();
+  const { handleRecentRounds } = useOptionsGrid();
+
+  const [showPopup, setShowPopup] = useState<boolean>(false); // 팝업 표시 여부
   const recentRoundsNum = 5;
   // 옵션 배열
   const options: Option[] = [
@@ -29,27 +30,29 @@ const OptionsGrid: React.FC = () => {
       rank: null,
       path: "/result",
       action: () => {
-        const recentRounds = getRecentRounds(recentRoundsNum);
-
-        const recentWinningRounds = new Set(
-          recentRounds.flatMap((round) => round.winningNumbers)
-        );
-
-        const requiredNumbers = [...recentWinningRounds]
-          .map(Number)
-          .sort((a, b) => a - b);
-
-        handleRequiredNumbers(requiredNumbers);
+        handleRecentRounds();
       },
     },
     { label: "제외 없이\n번호 생성", rank: "1위", path: "/result" },
-    { label: "수동으로\n번호 선택", rank: null, path: "/manual-selection" },
+    {
+      label: "최근 N회차\n최소 K개\n번호조합",
+      rank: null,
+      path: "",
+      action: () => {
+        setShowPopup(true);
+      },
+    },
     { label: "미 출현\n번호조합", rank: null, path: "/missing-numbers" },
     { label: "직전 회차\n번호제외", rank: null, path: "/exclude-last-round" },
     { label: "짝수 4개\n홀수 2개", rank: null, path: "/even-4-odd-2" },
     { label: "홀수 4개\n짝수 2개", rank: null, path: "/odd-4-even-2" },
     { label: "짝수 3개\n홀수 3개", rank: "3위", path: "/even-3-odd-3" },
   ];
+
+  // const handlePopupConfirm = (recentRounds: number, requiredCount: number) => {
+  //   setShowPopup(false);
+  //   navigate(`/result?n=${recentRounds}&k=${requiredCount}`); // N, K 값을 전달
+  // };
 
   return (
     <div className={styles.container}>
@@ -61,7 +64,7 @@ const OptionsGrid: React.FC = () => {
             className={styles.optionButton}
             onClick={() => {
               if (option.action) option.action(); // 작업 실행
-              navigate(option.path); // 경로 이동
+              if (option.path) navigate(option.path); // 경로 이동
             }}
           >
             <span>{option.label.replace("\\n", "\n")}</span>
@@ -71,16 +74,19 @@ const OptionsGrid: React.FC = () => {
           </div>
         ))}
       </div>
+      {showPopup && (
+        <PopupModal
+          onConfirm={() => {
+            handleRecentRounds();
+            navigate("/result");
+          }} // 팝업에서 입력한 값 처리
+          onClose={() => setShowPopup(false)} // 팝업 닫기
+        />
+      )}
       <p className={styles.note}>
         <span className={styles.highlight}>1~45</span>의 모든 번호에서
         생성합니다.
       </p>
-      <button
-        className={styles.confirmButton}
-        onClick={() => alert("현재 작업은 준비 중입니다.")}
-      >
-        확인 →
-      </button>
     </div>
   );
 };
