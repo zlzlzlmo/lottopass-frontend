186785dd6c72ce3352e5b06579d22453f3c1900c
seunghoon <zlzlzlmo@daum.net>
2024-12-26 13:10:51 +0900

    번호 생성 첫 진입시 상태값 초기화

--- src/pages/numberGeneration/NumberGeneration.tsx
+++ src/pages/numberGeneration/NumberGeneration.tsx
@@ -1,10 +1,17 @@
 // NumberGeneration.tsx
-import React from "react";
+import React, { useEffect } from "react";
 // import styles from "./NumberGeneration.module.scss";
 import Layout from "../../components/layout/Layout";
 import OptionsGrid from "./optionsGrid/OptionsGrid";
+import { useLottoNumber } from "../../context/lottoNumbers";
 
 const NumberGeneration: React.FC = () => {
+  const { resetNumbers } = useLottoNumber();
+
+  useEffect(() => {
+    resetNumbers();
+  }, [resetNumbers]);
+
   return (
     <Layout>
       <OptionsGrid />
