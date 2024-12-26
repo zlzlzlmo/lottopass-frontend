15772f694c186aacdb0d13580ae9af76f6484e7e
seunghoon <zlzlzlmo@daum.net>
2024-12-26 13:10:17 +0900

    번호 생성 커스텀 훅 로직 변경

--- src/context/lottoNumbers.tsx
+++ src/context/lottoNumbers.tsx
@@ -5,14 +5,19 @@ import React, {
   useMemo,
   useState,
 } from "react";
+import { getRandomNum, shuffle } from "../utils/number";
 
 interface LottoNumberContextType {
+  roundCount: number;
+  minimumRequiredCount: number;
   excludedNumbers: number[];
   requiredNumbers: number[];
   handleExcludedNumbers: (numbers: number[]) => void;
   handleRequiredNumbers: (numbers: number[]) => void;
   generateNumbers: () => number[]; // 결과 번호 생성 함수
   resetNumbers: () => void;
+  setRoundCount: React.Dispatch<React.SetStateAction<number>>;
+  setMinimumRequiredCount: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const LottoNumberContext = createContext<LottoNumberContextType | undefined>(
@@ -22,26 +27,45 @@ const LottoNumberContext = createContext<LottoNumberContextType | undefined>(
 export const LottoNumberProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const initialRoundCount = 5;
+  const initialMinimumRequiredCount = 6;
+
   const [excludedNumbers, setExcludedNumbers] = useState<number[]>([]);
   const [requiredNumbers, setRequiredNumbers] = useState<number[]>([]);
-  const [minimumRequiredCount, setMinimumRequiredCount] = useState<number>(3);
-  const [roundCount, setRoundCount] = useState<number>(5);
+  const [minimumRequiredCount, setMinimumRequiredCount] = useState<number>(
+    initialMinimumRequiredCount
+  );
+  const [roundCount, setRoundCount] = useState<number>(initialRoundCount);
 
-  // 제외된 번호를 뺀 나머지 번호들에서 번호 생성
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const generateNumbers = () => {
     const allNumbers = Array.from({ length: 45 }, (_, i) => i + 1);
 
-    const availableNumbers = allNumbers.filter((num) => {
-      if (requiredNumbers.length <= 0) return !excludedNumbers.includes(num);
-      return requiredNumbers.includes(num);
-    });
+    const randomCount = getRandomNum(minimumRequiredCount, 6);
+
+    const shuffledRequiredNumbers = shuffle(requiredNumbers);
+    const uniquerRequiredNumbers = [...new Set(shuffledRequiredNumbers)].slice(
+      0,
+      randomCount
+    );
+
+    const availableNumbers = new Set(
+      allNumbers.filter((num) => {
+        if (requiredNumbers.length <= 0) return !excludedNumbers.includes(num);
+        return uniquerRequiredNumbers.includes(num);
+      })
+    );
+
+    while (availableNumbers.size < 6) {
+      const randomNum = getRandomNum(1, 45);
+      availableNumbers.add(randomNum);
+    }
 
     const randomNumbers: number[] = [];
 
     while (randomNumbers.length < 6) {
-      const randomIndex = Math.floor(Math.random() * availableNumbers.length);
-      const number = availableNumbers[randomIndex];
+      const randomIndex = Math.floor(Math.random() * availableNumbers.size);
+      const number = [...availableNumbers][randomIndex];
       if (!randomNumbers.includes(number)) randomNumbers.push(number);
     }
 
@@ -50,22 +74,22 @@ export const LottoNumberProvider: React.FC<{ children: React.ReactNode }> = ({
 
   const handleExcludedNumbers = useCallback((numbers: number[]) => {
     setExcludedNumbers(numbers);
-    setRequiredNumbers([]); // 필수 번호 초기화
+    setRequiredNumbers([]);
   }, []);
 
   const handleRequiredNumbers = useCallback((numbers: number[]) => {
     setRequiredNumbers(numbers);
-    setExcludedNumbers([]); // 제외 번호 초기화
+    setExcludedNumbers([]);
   }, []);
 
   const resetNumbers = useCallback(() => {
     setExcludedNumbers([]);
     setRequiredNumbers([]);
-    setMinimumRequiredCount(3); // 기본값 초기화
-    setRoundCount(5); // 기본값 초기화
+    setMinimumRequiredCount(initialMinimumRequiredCount);
+    setRoundCount(initialRoundCount);
   }, []);
 
-  const value = useMemo(
+  const value = useMemo<LottoNumberContextType>(
     () => ({
       excludedNumbers,
       requiredNumbers,
