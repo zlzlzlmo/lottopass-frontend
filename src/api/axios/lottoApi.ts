425b3c8256fb75793e2b4609baf10227320d2f25
seunghoon <zlzlzlmo@daum.net>
2024-12-31 17:14:46 +0900

    api설정 변경

--- src/api/axios/lottoApi.ts
+++ src/api/axios/lottoApi.ts
@@ -1,9 +1,28 @@
-// import { FindAllResponse } from "lottopass-shared";
-import { FindAllResponse } from "lottopass-shared";
+// API 관련 코드
+import { FindAllResponse, LottoDraw } from "lottopass-shared";
 import { API_URLS } from "../../constants/apiUrls";
 import axiosInstance from "./axiosConfig";
 
-export const getAllRounds = async (): Promise<FindAllResponse> => {
-  const response = await axiosInstance.get(API_URLS.ALL_ROUNDS);
-  return response.data;
+export const getAllRounds = async (): Promise<FindAllResponse<LottoDraw[]>> => {
+  try {
+    const response = await axiosInstance.get(API_URLS.ALL_ROUNDS);
+    return response.data;
+  } catch (error) {
+    return {
+      status: "error",
+      message: (error as Error).message || "Failed to fetch all rounds",
+    };
+  }
+};
+
+export const getLatestRound = async (): Promise<FindAllResponse<LottoDraw>> => {
+  try {
+    const response = await axiosInstance.get(API_URLS.LATEST);
+    return response.data;
+  } catch (error) {
+    return {
+      status: "error",
+      message: (error as Error).message || "Failed to fetch the latest round",
+    };
+  }
 };
--- src/constants/apiUrls.ts
+++ src/constants/apiUrls.ts
@@ -1,3 +1,4 @@
 export const API_URLS = {
-  ALL_ROUNDS: "/lotto/all",
+  ALL_ROUNDS: "/all",
+  LATEST: "/latest",
 };
