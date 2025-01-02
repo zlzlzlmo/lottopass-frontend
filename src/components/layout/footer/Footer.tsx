8fc1d64e1d425a7752d6f0b38d4fade94bbd795b
seunghoon <zlzlzlmo@daum.net>
2025-01-02 23:09:05 +0900

    이메일 링크 추가

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -7,7 +7,12 @@ const Footer = () => {
         <p className={styles.companyName}>LottoPass © 2025</p>
       </div>
       <div className={styles.contact}>
-        <p>고객센터: zlzlzlmo60@gmail.com</p>
+        <p>
+          고객센터:{" "}
+          <a href="mailto:zlzlzlmo60@gmail.com" className={styles.emailLink}>
+            zlzlzlmo60@gmail.com
+          </a>
+        </p>
         <p>문의사항이 있다면 이메일로 연락주세요!</p>
       </div>
     </footer>
