ecd5884c500602bbfb2ce81351fbb7faaf9f0a3e
seunghoon <zlzlzlmo@daum.net>
2025-01-05 04:57:57 +0900

    NumberActions 리팩토링

--- src/pages/numberGeneration/NumberGeneration.tsx
+++ src/pages/numberGeneration/NumberGeneration.tsx
@@ -1,10 +1,19 @@
+import { useAppDispatch } from "@/redux/hooks";
 import Layout from "../../components/layout/Layout";
-import OptionsGrid from "./optionsGrid/OptionsGrid";
+import { useEffect } from "react";
+import { fetchAllDraws } from "@/features/draw/drawSlice";
+import { NumberActionButtons } from "./components";
 
 const NumberGeneration: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllDraws());
+  }, []);
   return (
     <Layout>
-      <OptionsGrid />
+      <NumberActionButtons />
+      {/* <OptionsGrid /> */}
     </Layout>
   );
 };
