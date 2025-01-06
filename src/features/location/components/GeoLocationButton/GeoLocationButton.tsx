0b6f615f40f93013eedb89798932393fcdb2232a
seunghoon <zlzlzlmo@daum.net>
2025-01-06 14:38:41 +0900

    에러 유틸 생성

--- src/features/location/components/GeoLocationButton/GeoLocationButton.tsx
+++ src/features/location/components/GeoLocationButton/GeoLocationButton.tsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
-import { Button, message, Space, Typography } from "antd";
+import { Button, Space, Typography } from "antd";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { setAddress, setError, setLocation } from "../../locationSlice";
 import { CompassOutlined } from "@ant-design/icons";
 import { locationService } from "@/api";
+import { showError, getErrorMessage } from "@/utils/error";
 
 const { Text } = Typography;
+
+const locationErrorMessages = [
+  { code: 1, message: "위치 정보 접근이 거부되었습니다." },
+  { code: 2, message: "위치 정보를 사용할 수 없습니다." },
+  { code: 3, message: "위치 정보를 가져오는 데 실패했습니다." },
+  { code: 0, message: "알 수 없는 오류가 발생했습니다." },
+];
 
 interface GeoLocationButtonProps {
   onLocationSelect: (province: string, city: string) => void;
@@ -18,50 +26,6 @@ const GeoLocationButton: React.FC<GeoLocationButtonProps> = ({
   const myLocation = useAppSelector((state) => state.location.myLocation);
   const myAddress = useAppSelector((state) => state.location.myAddress);
   const [isFetching, setIsFetching] = useState(false);
-
-  const provinceMapping: Record<string, string> = {
-    경기도: "경기",
-    서울특별시: "서울",
-    부산광역시: "부산",
-    강원도: "강원특별자치도",
-    // 추가 매핑...
-  };
-
-  // 도 매칭 함수
-  const matchProvince = (province: string): string => {
-    return provinceMapping[province] || province; // 매칭되지 않으면 원본 반환
-  };
-
-  type AddressParts = {
-    province: string; // 도/특별시
-    city: string; // 시/군/구
-  };
-
-  const parseAddress = (address: string): AddressParts => {
-    const parts = address.split(" ");
-
-    // 도와 시/구로 분리
-    const province = parts[0]; // 첫 번째 값은 도
-    let city = "";
-
-    if (parts.length > 2) {
-      // 시/군/구가 2개 이상의 값으로 이루어진 경우
-      city = `${parts[1]} ${parts[2]}`;
-    } else {
-      // 시/군/구가 하나의 값으로만 이루어진 경우
-      city = parts[1];
-    }
-
-    return { province, city };
-  };
-
-  const showError = (errorMessage: string) => {
-    message.error({
-      content: errorMessage,
-      duration: 3, // 3초 후 자동 사라짐
-      style: { marginTop: "10px" }, // 페이지 상단 간격 조정
-    });
-  };
 
   const handleGetLocation = () => {
     if (!navigator.geolocation) {
@@ -84,29 +48,17 @@ const GeoLocationButton: React.FC<GeoLocationButtonProps> = ({
           longitude,
         });
 
-        const { province, city } = parseAddress(address);
+        const parts = address.split(" ");
+        const province = parts[0];
+        const city = parts.length > 2 ? `${parts[1]} ${parts[2]}` : parts[1];
 
-        // const matchedProvince = matchProvince(province);
-        onLocationSelect(matchProvince(province), city);
+        onLocationSelect(province, city);
         dispatch(setAddress(address));
       },
       (error) => {
-        let errorMessage = "";
-        switch (error.code) {
-          case error.PERMISSION_DENIED:
-            errorMessage = "위치 정보 접근이 거부되었습니다.";
-            break;
-          case error.POSITION_UNAVAILABLE:
-            errorMessage = "위치 정보를 사용할 수 없습니다.";
-            break;
-          case error.TIMEOUT:
-            errorMessage = "위치 정보를 가져오는 데 실패했습니다.";
-            break;
-          default:
-            errorMessage = "알 수 없는 오류가 발생했습니다.";
-        }
+        const errorMessage = getErrorMessage(error.code, locationErrorMessages);
         dispatch(setError(errorMessage));
-        showError(errorMessage); // 상단에 에러 메시지 표시
+        showError(errorMessage); // 에러 표시
         setIsFetching(false);
       }
     );
@@ -115,7 +67,6 @@ const GeoLocationButton: React.FC<GeoLocationButtonProps> = ({
   return (
     <div style={{ marginTop: 16 }}>
       <Space direction="vertical" style={{ width: "100%" }}>
-        {/* 위치 가져오기 및 재설정 버튼 */}
         <Button
           icon={<CompassOutlined />}
           type={myLocation ? "dashed" : "default"}
@@ -125,7 +76,6 @@ const GeoLocationButton: React.FC<GeoLocationButtonProps> = ({
           {myLocation ? "위치 재설정하기" : "내 위치 가져오기"}
         </Button>
 
-        {/* 현재 위치 표시 */}
         {myLocation && (
           <Text type="secondary" style={{ fontSize: "12px", display: "block" }}>
             현재 위치:{" "}
