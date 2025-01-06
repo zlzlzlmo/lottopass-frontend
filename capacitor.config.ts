d48ba5a09c995689e74a6787565800499d3bac4a
seunghoon <zlzlzlmo@daum.net>
2025-01-06 21:07:46 +0900

    Capacitor 설치

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.lottopass.app",
+  appName: "lotto-pass-frontend",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
