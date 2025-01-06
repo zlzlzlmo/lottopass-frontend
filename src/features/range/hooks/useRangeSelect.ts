abe8cd2505a682eef185d11bd9a863abc3bf9469
seunghoon <zlzlzlmo@daum.net>
2025-01-07 00:20:13 +0900

    useRangeSelector 커스텀훅 생성

---
+++ src/features/range/hooks/useRangeSelect.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+interface UseRangeSelectorProps<T> {
+  data: T[];
+  getDrawNumber: (item: T) => number;
+}
+
+export const useRangeSelector = <T>({
+  data,
+  getDrawNumber,
+}: UseRangeSelectorProps<T>) => {
+  const [range, setRange] = useState<[number, number] | null>(null);
+
+  const drawNumbers = data.map(getDrawNumber);
+  const maxDraw = drawNumbers.length > 0 ? Math.max(...drawNumbers) : 0;
+  const minDraw = drawNumbers.length > 0 ? Math.min(...drawNumbers) : 0;
+
+  useEffect(() => {
+    if (data.length > 0 && range === null) {
+      setRange([Math.max(minDraw, maxDraw - 50), maxDraw]);
+    }
+  }, [data, range, minDraw, maxDraw]);
+
+  const filteredDraws = range
+    ? data.filter(
+        (item) =>
+          getDrawNumber(item) >= range[0] && getDrawNumber(item) <= range[1]
+      )
+    : [];
+
+  const handleRangeChange = (value: [number, number]) => {
+    setRange(value);
+  };
+
+  return {
+    filteredDraws,
+    range,
+    setRange,
+    minDraw,
+    maxDraw,
+    handleRangeChange,
+  };
+};
