29f5dc7946f536a6ddae30af3eef0c942c9d6fb7
seunghoon <zlzlzlmo@daum.net>
2025-01-08 00:46:07 +0900

    authSlice 등록

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface User {
+  email: string;
+  firstName: string;
+  lastName: string;
+  picture: string;
+}
+
+interface AuthState {
+  user: User | null;
+  isLoggedIn: boolean;
+}
+
+const initialState: AuthState = {
+  user: null,
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+      state.isLoggedIn = true;
+    },
+    clearUser: (state) => {
+      state.user = null;
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const { setUser, clearUser } = authSlice.actions;
+export default authSlice.reducer;
