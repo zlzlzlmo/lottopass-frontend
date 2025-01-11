444b27a8fdc63b079a328233c159d6d945441bdd
seunghoon <zlzlzlmo@daum.net>
2025-01-11 14:51:07 +0900

    로띠 최적화

--- src/pages/home/hero/lottie/index.ts
+++
@@ -1,9 +0,0 @@
-import store from "./store-lottie.json";
-import simulation from "./simulation-lottie.json";
-import statistic from "./statistic-lottie.json";
-import lotto from "./lotto-lottie.json";
-
-export const storeLottie = store;
-export const simulationLottie = simulation;
-export const statisticLottie = statistic;
-export const lottoLottie = lotto;
