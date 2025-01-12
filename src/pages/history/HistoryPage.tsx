c9ca1cf784205cdf4ebf1dadf52a9c6fd4bf5bb4
seunghoon <zlzlzlmo@daum.net>
2025-01-12 10:56:57 +0900

    모든 회차페이지 하드코딩 제거

--- src/pages/history/HistoryPage.tsx
+++ src/pages/history/HistoryPage.tsx
@@ -44,7 +44,7 @@ const HistoryPage: React.FC = () => {
       <Container>
         <Banner>
           🎉 역대 로또 회차 정보를 확인해보세요! <br />
-          <strong>1,153회</strong> 데이터가 제공됩니다.
+          <strong>{allDraws.length}회</strong> 데이터가 제공됩니다.
         </Banner>
 
         <div className={styles.cards}>
